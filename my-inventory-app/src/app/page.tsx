a29a8b6cb1de57ce514b790bbbb0bda249129aab
Siddhartha Shakya <sshakya@dons.usfca.edu>
2025-09-18 21:22:17 -0700

    Addign descrption

--- my-inventory-app/src/app/page.tsx
+++ my-inventory-app/src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <>
       <h1>Inventory Tracker for Faire and Etsy</h1>
+      <p>This site alows you to check the inventory acorss multipe platforms to make sure you always know your inventory</p>
     </>
   );
 }
